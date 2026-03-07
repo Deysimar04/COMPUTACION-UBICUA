ca2a973972b344fdbedf194e977689c1250220ad
Deysimar04 <alejandraarboleda053@gmail.com>
2026-03-06 21:19:48 -0500

    Update LM35.ino

--- LM35+LED/LM35.ino
+++ LM35+LED/LM35.ino
@@ -1,1 +1,27 @@
+int sensorPin = A0;
+int ledPin = 9;
+float temperatura;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+
+  int valor = analogRead(sensorPin);
+  float voltaje = valor * (5.0 / 1023.0);
+  temperatura = voltaje * 100;
+
+  Serial.print("Temperatura: ");
+  Serial.println(temperatura);
+
+  if (temperatura > 23) {   // umbral más bajo
+    digitalWrite(ledPin, HIGH);
+  } 
+  else {
+    digitalWrite(ledPin, LOW);
+  }
+
+  delay(300);
+}
